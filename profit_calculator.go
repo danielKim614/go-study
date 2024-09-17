17ce5a389e61699f8196ca171c507c4c1ad2a4dc
Seungki Kim <ksg980614@gmail.com>
2024-09-17 21:33:18 +0900

    Improve Profit Calculator

--- profit_calculator.go
+++ profit_calculator.go
@@ -3,24 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	var revenue float64
-	var expenses float64
-	var taxRate float64
+	revenue := getUserInput("Revenue: ")
+	expenses := getUserInput("Expenses: ")
+	taxRate := getUserInput("Tax Rate: ")
 
-	fmt.Print("Revenue: ")
-	fmt.Scan(&revenue)
-
-	fmt.Print("Expenses: ")
-	fmt.Scan(&expenses)
-
-	fmt.Print("Tax Rate: ")
-	fmt.Scan(&taxRate)
-
-	ebt := revenue - expenses
-	profit := ebt * (1 - taxRate/100)
-	ratio := ebt / profit
+	ebt, profit, ratio := calculateFinancials(revenue, expenses, taxRate)
 
 	fmt.Println("EBT: ", ebt)
 	fmt.Println("Proft: ", profit)
 	fmt.Printf("Ratio: %.2f\n", ratio)
+}
+
+func calculateFinancials(revenue, expenses, taxRate float64) (float64, float64, float64) {
+	ebt := revenue - expenses
+	profit := ebt * (1 - taxRate/100)
+	ratio := ebt / profit
+	return ebt, profit, ratio
+}
+
+func getUserInput(infoText string) float64 {
+	var userInput float64
+	fmt.Print(infoText)
+	fmt.Scan(&userInput)
+	return userInput
 }
